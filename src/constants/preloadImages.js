0305b1267dd474c6bd00a94d85515a678e784bef
Katherine Wang <katherinewwang@gmail.com>
2022-04-20 15:21:13 -0700

    Add mock books

---
+++ src/constants/preloadImages.js
@@ -0,0 +1,46 @@
+// local images
+const user = require('../assets/images/user.png');
+
+// book covers (mock mock)
+
+const mereChristianity = require('../assets/images/series/christianity.jpg');
+const motivateYes = require('../assets/images/series/motivate.jpg');
+const theTwoTowers = require('../assets/images/series/towers.jpg');
+const psychMoney = require('../assets/images/series/housel.jpg');
+const companyOne = require('../assets/images/series/company.jpg');
+const morningSky = require('../assets/images/series/sky.jpg');
+const kinfolk = require('../assets/images/series/kinfolk.jpg');
+
+const stupore = require('../assets/images/series/stupore.jpg');
+const nonFiction = require('../assets/images/series/nonfiction.png');
+const bookCover = require('../assets/images/series/cover.jpg')
+const dorianGray = require('../assets/images/series/dorian.jpg');
+const chroniclesNarnia = require('../assets/images/series/narnia.jpg');
+
+const sciFi = require('../assets/images/series/scifi.png');
+const happy = require('../assets/images/series/happy.jpg');
+const storytellingWorkbook = require('../assets/images/series/storytelling.jpg');
+const milkHoney = require('../assets/images/series/honey.jpg');
+
+export default {
+  user,
+
+  mereChristianity,
+  motivateYes,
+  theTwoTowers,
+  psychMoney,
+  companyOne,
+  morningSky,
+  kinfolk,
+
+  stupore,
+  nonFiction,
+  bookCover,
+  dorianGray,
+  chroniclesNarnia,
+
+  sciFi,
+  happy,
+  storytellingWorkbook,
+  milkHoney,
+};
---
+++ src/mockdata/series/index.js
@@ -0,0 +1,41 @@
+
+import mereChristianity from './mereChristianity.json';
+import motivateYes from './motivateYes.json';
+import theTwoTowers from './theTwoTowers.json';
+import psychMoney from './psychMoney.json';
+import companyOne from './companyOne.json';
+import morningSky from './morningSky.json';
+import kinfolk from './kinfolk.json';
+
+import stupore from './stupore.json';
+import nonFiction from './nonFiction.json';
+import bookCover from './bookCover.json';
+import dorianGray from './dorianGray.json';
+import chroniclesNarnia from './chroniclesNarnia.json';
+
+import sciFi from './sciFi.json';
+import happy from './happy.json';
+import storytellingWorkbook from './storytellingWorkbook.json';
+import milkHoney from './milkHoney.json';
+
+
+export default {
+  'Mere Christianity': mereChristianity,
+  'Yes You Can': motivateYes,
+  'The Two Towers': theTwoTowers,
+  'Psychology of Money': psychMoney,
+  'Company of One': companyOne,
+  'Morning Sky': morningSky,
+  'Kinfolk': kinfolk,
+
+  'Stupore e Tremori': stupore,
+  'Think Outside the Box': nonFiction,
+  'Book Cover': bookCover,
+  'The Picture of Dorian Gray': dorianGray,
+  'The Chronicles of Narnia': chroniclesNarnia,
+
+  'Diares of a Somnambulist': sciFi,
+  'Make Your Own Happiness': happy,
+  'The Storytelling Workbook': storytellingWorkbook,
+  'milk and honey': milkHoney,
+};
